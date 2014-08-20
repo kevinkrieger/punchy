2e8094cdd3c91b913333f02dc9fa792eabde8c60
Kevin Krieger <kevinjkrieger@gmail.com>
2014-08-19 23:43:00 -0600

    Fixed i2c library... now works with mpu6050 dmp init

--- punchy/i2c.c
+++ punchy/i2c.c
@@ -47,6 +47,7 @@ void i2c_transmit(void)   // txifg will go right away, so we need to disable int
     __bis_SR_register(LPM0_bits + GIE);
     // __bis_SR_register(GIE);
     //UCB0CTL1 |= UCTR + UCTXSTT;
+    while(UCB0STAT & UCBBUSY); /* NEED TO DO THIS!!!! */
 }
 
 void i2c_transmit_to_receive()
@@ -58,6 +59,7 @@ void i2c_transmit_to_receive()
 
 //__eint();
     __bis_SR_register(LPM0_bits + GIE);
+
 }
 
 void i2c_receive(void)
@@ -76,23 +78,181 @@ void i2c_multireceive(uint8_t receive_amount)
 {
     // Last one needs to have STP bit set in middle of reception
     //mpu6050_buffer_pointer=0;
-    int i;
-    i2c_rx_buffer_pointer = 0;
-    while(UCB0CTL1 & UCTXSTP);
-    UCB0CTL1 &= ~UCTR;
-    UCB0CTL1 |= UCTXSTT;
-    for( i = 0; i<receive_amount - 1; i++)
-    {
+    if(receive_amount == 1) {
+        i2c_receive();
+    } else {
+        int i;
+        i2c_rx_buffer_pointer = 0;
+        while(UCB0CTL1 & UCTXSTP);
+        UCB0CTL1 &= ~UCTR;
+        UCB0CTL1 |= UCTXSTT;
+        for( i = 0; i<receive_amount - 1; i++)
+        {
+            __bis_SR_register(LPM0_bits + GIE);
+            //		IFG2 &= ~UCB0TXIFG; 		// clear txifg
+            // RX interrupt
+            // will fire when we have received a byte
+        }
+    //	while(UCB0CTL1 &UCTXSTP);
+    //	UCB0CTL1 &= ~UCTR;
+    //	UCB0CTL1 |= UCTXSTT;
+        //while(UCB0CTL1 & UCTXSTT);
+        UCB0CTL1 |= UCTXSTP;
         __bis_SR_register(LPM0_bits + GIE);
-        //		IFG2 &= ~UCB0TXIFG; 		// clear txifg
-        // RX interrupt
-        // will fire when we have received a byte
     }
-//	while(UCB0CTL1 &UCTXSTP);
-//	UCB0CTL1 &= ~UCTR;
-//	UCB0CTL1 |= UCTXSTT;
-    //while(UCB0CTL1 & UCTXSTT);
-    UCB0CTL1 |= UCTXSTP;
-    __bis_SR_register(LPM0_bits + GIE);
 }
 
+
+void i2c_write_reg(uint8_t reg, uint8_t value) {
+    i2c_tx_buffer[1] = reg;
+    i2c_tx_buffer[0] = value;
+	i2c_tx_buffer_counter =2;
+	i2c_transmit();
+}
+
+/* Write bytes to a single register address */
+void i2c_write_bytes(uint8_t regAddr, uint8_t length, uint8_t* data) {
+    //I2C_writeBytesToAddress(devAddr, regAddr, length, data);
+  /*  int i;
+    for(i = 0; i<length; i++) {
+        i2c_write_reg(regAddr,*data);
+        data++;
+    }*/
+   // if(length == 1) {
+   //     i2c_write_reg(regAddr,*data);
+   // } else {
+        i2c_tx_buffer[length] = regAddr;
+        for(int i = 0;i<length;i++) {
+            i2c_tx_buffer[length-i-1] = data[i];
+        }
+        i2c_tx_buffer_counter = length+1;
+        i2c_transmit();
+    //}
+}
+
+void i2c_read_reg(uint8_t reg) {
+    i2c_tx_buffer[0] = reg;
+	i2c_tx_buffer_counter = 1;
+	//i2c_transmit_to_receive();
+	i2c_transmit();
+	i2c_receive();
+}
+
+/** Read multiple bytes from an 8-bit device register.
+ * @param regAddr First register regAddr to read from
+ * @param length Number of bytes to read
+ * @param data Buffer to store read data in
+ */
+void i2c_read_bytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
+  /*  for (uint8_t k = 0; k < length; k++) {
+        i2c_read_reg(regAddr);
+        data[k] = i2c_rx_buffer[0];
+    }*/
+    // Need to do a multireceive...
+    if(length < 1) {
+        return;
+    }
+    i2c_tx_buffer[0] = regAddr;
+    i2c_tx_buffer_counter = 1;
+    i2c_transmit();
+    i2c_multireceive(length);
+     for (uint8_t k = 0; k < length; k++) {
+        //i2c_read_reg(regAddr);
+        data[k] = i2c_rx_buffer[k];
+    }
+}
+
+/** Read multiple bits from an 8-bit device register.
+ * @param regAddr Register regAddr to read from
+ * @param bitStart First bit position to read (0-7)
+ * @param length Number of bits to read (not more than 8)
+ */
+void i2c_read_bits(uint8_t regAddr, uint8_t bitStart, uint8_t length) {
+    // 01101001 read byte
+    // 76543210 bit numbers
+    //    xxx   args: bitStart=4, length=3
+    //    010   masked
+    //   -> 010 shifted
+    uint8_t b;
+    i2c_read_reg(regAddr);
+    b = i2c_rx_buffer[0];
+    if(b != 0) {
+        uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+        b &= mask;
+        b >>= (bitStart - length + 1);
+    }
+    i2c_rx_buffer[0] = b;
+//    if ((count = i2c_read_reg(regAddr)) != 0) {
+}
+
+/** Write multiple words to a 16-bit device register.
+ * @param regAddr First register address to write to
+ * @param length Number of words to write
+ * @param data Buffer to copy new data from
+ * @return Status of operation (true = success)
+ */ //0x13,1, (0)
+bool i2c_write_words(uint8_t regAddr, uint8_t length, uint16_t* data) {
+    i2c_tx_buffer_counter = 1;
+    i2c_tx_buffer[0] = regAddr;
+    i2c_transmit();
+
+    for (uint8_t i = 0; i < length*2 ; i++) {
+        i2c_tx_buffer_counter = 2;
+        i2c_tx_buffer[1] = (uint8_t)(data[i++]>>8);
+        i2c_tx_buffer[0] = (uint8_t)(data[i++]);
+        i2c_transmit();
+    }
+}
+
+/** Write single word to a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register address to write to
+ * @param data New word value to write
+ * @return Status of operation (true = success)
+ */
+bool i2c_write_word(uint8_t regAddr, uint16_t data) {
+    return i2c_write_words(regAddr, 1, &data);
+}
+
+/** write a single bit in an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to write to
+ * @param bitNum Bit position to write (0-7)
+ * @param value New bit value to write
+ * @return Status of operation (true = success)
+ */
+bool i2c_write_bit(uint8_t regAddr, uint8_t bitNum, uint8_t data) {
+    uint8_t b;
+    i2c_read_reg(regAddr);
+
+    b = (data != 0) ? (i2c_rx_buffer[0] | (1 << bitNum)) : (i2c_rx_buffer[0] & ~(1 << bitNum));
+    i2c_write_reg(regAddr, b);
+    return true;
+}
+
+
+/** Write multiple bits in an 8-bit device register.
+ * @param regAddr Register regAddr to write to
+ * @param bitStart First bit position to write (0-7)
+ * @param length Number of bits to write (not more than 8)
+ * @param data Right-aligned value to write
+ * @return Status of operation (true = success)
+ */
+bool i2c_write_bits(uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data) {
+    //      010 value to write
+    // 76543210 bit numbers
+    //    xxx   args: bitStart=4, length=3
+    // 00011100 mask byte
+    // 10101111 original value (sample)
+    // 10100011 original & ~mask
+    // 10101011 masked | value
+    uint8_t b;
+    i2c_read_reg(regAddr);
+    b = i2c_rx_buffer[0];
+    uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+    data <<= (bitStart - length + 1); // shift data into correct position
+    data &= mask; // zero all non-important bits in data
+    b &= ~(mask); // zero all important bits in existing byte
+    b |= data; // combine data with existing byte
+    i2c_write_reg(regAddr, b);
+ }
--- punchy/i2c.h
+++ punchy/i2c.h
@@ -5,14 +5,20 @@
 #include <msp430g2553.h>
 
 /* Defines */
+
+typedef int bool;
+#define true 1
+#define false 0
+
 #define SDA			BIT7
 #define SCL			BIT6
 
 /* Data */
 uint8_t tx_to_rc;
-uint8_t i2c_rx_buffer[16];
+uint8_t i2c_rx_buffer[32];
 uint8_t i2c_rx_buffer_pointer;
-uint8_t i2c_tx_buffer[16];
+uint8_t i2c_rx_buffer_length;
+uint8_t i2c_tx_buffer[32];
 uint8_t i2c_tx_buffer_counter;
 
 
@@ -27,6 +33,23 @@ void i2c_transmit_to_receive(void);
 
 void i2c_multireceive(uint8_t amount);
 
+void i2c_read_reg(uint8_t reg);
+
+void i2c_write_reg(uint8_t reg, uint8_t value);
+
+void i2c_read_bits(uint8_t reg,uint8_t bitStart, uint8_t length);
+
+bool i2c_write_bit(uint8_t regAddr, uint8_t bitNum, uint8_t data);
+void i2c_write_bytes(uint8_t regAddr, uint8_t length, uint8_t* data);
+void i2c_read_bytes(uint8_t regAddr, uint8_t length, uint8_t *data);
+bool i2c_write_bits(uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data);
+/** Write single word to a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register address to write to
+ * @param data New word value to write
+ * @return Status of operation (true = success)
+ */
+bool i2c_write_word(uint8_t regAddr, uint16_t data);
 static inline void i2c_handle_state_interrupt(void) {
 	if(UCB0STAT & UCNACKIFG) {
         //Need to send stop
@@ -51,8 +74,25 @@ static inline void i2c_handle_TXRX_interrupt(void) {
     }
 	*///	mpu6050_buffer[mpu6050_buffer_pointer++] = UCB0RXBUF;
 		//if(mpu6050_buffer_pointer == 16) mpu6050_buffer_pointer = 0;
-		i2c_rx_buffer[i2c_rx_buffer_pointer++] = UCB0RXBUF;
-		if(i2c_rx_buffer_pointer == 16) i2c_rx_buffer_pointer = 0;
+		/*i2c_rx_buffer_length--;
+		if(i2c_rx_buffer_length) {
+            if(i2c_rx_buffer_length==1) {
+                // send stop and wait for it to finish
+                UCB0CTL1 |= UCTXSTP;
+                while(UCB0CTL1 & UCTXSTP);
+            } else {
+                // read byte
+                i2c_rx_buffer[i2c_rx_buffer_pointer++] = UCB0RXBUF;
+                if(i2c_rx_buffer_pointer == 32) i2c_rx_buffer_pointer = 0;
+            }
+		} else {
+            //read byte
+            i2c_rx_buffer[i2c_rx_buffer_pointer++] = UCB0RXBUF;
+            if(i2c_rx_buffer_pointer == 32) i2c_rx_buffer_pointer = 0;
+		}*/
+            i2c_rx_buffer[i2c_rx_buffer_pointer++] = UCB0RXBUF;
+            if(i2c_rx_buffer_pointer == 32) i2c_rx_buffer_pointer = 0;
+
 //		__bic_SR_register_on_exit(LPM0_bits);
 	// I2c state machine
 //		mpu6050_buffer[mpu6050_buffer_pointer] = UCB0RXBUF;
@@ -80,12 +120,12 @@ static inline void i2c_handle_TXRX_interrupt(void) {
 			//TXByteCounter--;
 			i2c_tx_buffer_counter--;
 //			__bic_SR_register_on_exit(LPM0_bits);
-		} else if (i2c_tx_buffer_counter == 1){
+		/*} else if (i2c_tx_buffer_counter == 1){
 			//UCB0TXBUF = mpu6050_tx_buffer[0];
 			UCB0TXBUF = i2c_tx_buffer[0];
 			UCB0CTL1 |= UCTXSTP;
 			IFG2 &= ~UCB0TXIFG;
-//			__bic_SR_register_on_exit(LPM0_bits);
+//			__bic_SR_register_on_exit(LPM0_bits);*/
 		} else {// don't stop if we are receiving next
 			if(tx_to_rc) {
 				IFG2 &= ~UCB0TXIFG;
