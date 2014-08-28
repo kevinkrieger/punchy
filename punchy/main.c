6521dae82ed64872012d52953ca291565f104111
Kevin Krieger <kevinjkrieger@gmail.com>
2014-08-28 00:53:06 -0600

    cleaned up comments, added motion detect for 200 samples, changed accel to +/- 16g.

--- punchy/main.c
+++ punchy/main.c
@@ -156,7 +156,7 @@ int main(void) {
                     mpu6050_setMotionDetectionDuration(40);
                     mpu6050_setIntEnabled(0x40);//motion detect... based on the product specification document, I don't think motion detect can generate an interrupt on INT pin,
                     // so we also set the data ready interrupt.
-
+                   mpu6050_configAccel(MPU6050_ACCEL_FS_16<<(MPU6050_ACONFIG_AFS_SEL_BIT-1));
                     data_received = 0;
                     P2DIR &= ~MPU6050_INT;  // Input
                     P2SEL &= ~MPU6050_INT;  // Digital IO Psel and psel2 are 0
@@ -188,96 +188,20 @@ int main(void) {
             }
 		}
 		if(mpu6050_interrupt) {
- /*           fifoCount = mpu6050_getFIFOCount();
-            sprintf(tempbuf,"F: %X\r\n",fifoCount);
-            hc05_transmit(tempbuf,strlen(tempbuf));
-            mpuIntStatus = mpu6050_getIntStatus();
-            if(fifoCount > 1024 || mpuIntStatus & 0x10) {
-                mpu6050_resetFIFO();
-                hc05_transmit("FO\r\n",4);
-            } else if(mpuIntStatus & 0x02) {
-                while(fifoCount < packetSize) fifoCount = mpu6050_getFIFOCount();
-
-                mpu6050_getFIFOBytes(fifoBuffer,packetSize);
-                fifoCount -= packetSize;
-                                // display quaternion values in InvenSense Teapot demo format:
-                teapotPacket[2] = fifoBuffer[0];
-                teapotPacket[3] = fifoBuffer[1];
-                teapotPacket[4] = fifoBuffer[4];
-                teapotPacket[5] = fifoBuffer[5];
-                teapotPacket[6] = fifoBuffer[8];
-                teapotPacket[7] = fifoBuffer[9];
-                teapotPacket[8] = fifoBuffer[12];
-                teapotPacket[9] = fifoBuffer[13];
-                //Serial.write(teapotPacket, 14);
-                hc05_transmit(teapotPacket,14);
-                teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-            }
-   */
             if(dmp_mode) {
                 mpuIntStatus = mpu6050_getIntStatus();
-               // hc05_transmit(&mpuIntStatus,1);
                 fifoCount = mpu6050_getFIFOCount();
-               // sprintf(tempbuf,"FC: %X\r\n",fifoCount);
-               // hc05_transmit(tempbuf,strlen(tempbuf));
                 if(fifoCount > 16) {
                     fifoCount =16;
                 }
                 mpu6050_getFIFOBytes(mpu6050_buffer,fifoCount);
                /* This seems to keep the fifo operating. I probably need to read the fifo faster so it doesn't 'die' on me */
                 mpu6050_resetFIFO();
-                //for(int i = 0; i<fifoCount;i++) {
-               //     sprintf(tempbuf,"%X ",mpu6050_buffer[i]);
-                 //   hc05_transmit(tempbuf,3);
-                //}
 
                 /* From J.Rowberg's library, the dmp packet output is:
                 bytes 0-15 quaternion (32 bits)  (w,x,y,z) but just use the first two bytes as 16 bit number
                 bytes 16-27 gyro (32 bits) (gx,gy,gz) but just use the first two bytes as 16 bit number
                 bytes 28-39 acceleration (32 bits) (ax,ay,az) but just use the first two bytes as 16 bit number
-
-                0 q[0] 32:24
-                1 q[0] 23:16
-                2 q[0] 15:8
-                3 q[0] 7:0
-                4 q[1]
-                5 .
-                6 .
-                7 .
-                8 q[2]
-                9 .
-                10 .
-                11 .
-                12 q[3]
-                13 .
-                14 .
-                15 .
-
-                16 g[0] 32:24
-                17 g[0] .
-                18 g[0]
-                19 .
-                20 g[2]
-                21 g[2]
-                22
-                23
-                24 g[3]
-                25 g[3]
-                26
-                27
-
-                28 a[0]
-                29 a[0]
-                30
-                31
-                32 a[1]
-                33 a[1]
-                34
-                35
-                36 a[2]
-                37 a[2]
-                38
-                39
                 */
 
                 teapotPacket[2] = mpu6050_buffer[0];
@@ -288,19 +212,32 @@ int main(void) {
                 teapotPacket[7] = mpu6050_buffer[9];
                 teapotPacket[8] = mpu6050_buffer[12];
                 teapotPacket[9] = mpu6050_buffer[13];
-                teapotPacket[10] = mpuIntStatus;
+                teapotPacket[10] = mpuIntStatus; // I modified the packet to sent the interrupt status in this byte
                 hc05_transmit(teapotPacket,14);
                  teapotPacket[11]++;
                 mpu6050_interrupt = 0;
+
             } else if (motion_detect_mode) {
                 if(mpu6050_getIntStatus() & 0x40) {
+                    // Disable motion interrupts, get accel and gyro values until they aren't interesting
+                    // anymore, then quit and enable interrupt.
+                    mpu6050_setIntEnabled(0x00);
                     //motion interrupt
-                    hc05_transmit("Motion\r\n",8);
+                  //  hc05_transmit("Motion\r\n",8);
+                    for(uint8_t j = 0; j<200; j++) {
+                        mpu6050_accel();
 
+                        //__delay_us(1000);
+                        delay_ms(1); // For some reason, using this delay_ms(5) or 1 ms, the output in mpu6050_accel() is not right...
+                        // mostly the \r\n are cut off. I have no idea why, perhaps due to modifying the smclk? a test will be to wait at
+                        // the end of the transmit function!
+
+                    }
+
+                    mpu6050_setIntEnabled(0x40);
                 }
             }
 		}
-
 		  __bis_SR_register(LPM1_bits + GIE);
 	}
 }
